091cfccc45a3e5a6d8e0d47cc5978f5a968da3e2
Shuhei Abe <blueletter123456789@gmail.com>
2022-01-22 18:46:52 +0900

    create pytest fixture

--- my_test/calculation.py
+++ my_test/calculation.py
@@ -1,3 +1,5 @@
+import os
+
 class Cal(object):
     def add_num_and_double(self, x, y):
         if type(x) is not int or type(y) is not int:
@@ -5,3 +7,10 @@ class Cal(object):
         result = x + y
         result *= 2
         return result
+
+    def save(self, file_dir, file_name):
+        if not os.path.exists(file_dir):
+            os.mkdir(file_dir)
+        file_path = os.path.join(file_dir, file_name)
+        with open(file_path, 'w+') as f:
+            f.write('test')
--- my_test/my_pytest2.py
+++ my_test/my_pytest2.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from my_test import calculation
 
@@ -6,14 +7,16 @@ class TestCal(object):
     @classmethod
     def setup_class(cls):
         cls.cal = calculation.Cal()
+        cls.file_name = 'test.txt'
 
-    def test_add_num_and_double(self, request):
-        os_name = request.config.getoption('--os-name')
-        print(os_name)
-        if os_name == 'mac':
-            print('ls')
-        elif os_name == 'windows':
-            print('dir')
-        else:
-            print('ls')
+    def test_add_num_and_double(self, csv_file):
+        print(csv_file)
         assert self.cal.add_num_and_double(1, 1) == 4
+
+    def test_save(self, tmpdir):
+        self.cal.save(tmpdir, self.file_name)
+        test_file_path = os.path.join(tmpdir, self.file_name)
+        # assert os.path.exists(test_file_path)
+        with open(test_file_path, 'r') as f:
+            text = f.read()
+        assert text == 'test'
