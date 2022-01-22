5f7263e4da7de77564da94a73596a0a430c614f5
Shuhei Abe <blueletter123456789@gmail.com>
2022-01-22 14:16:06 +0900

    create logging format

--- my_logging/my_logging.py
+++ my_logging/my_logging.py
@@ -2,12 +2,15 @@ import logging
 
 
 def main():
-    logging.basicConfig(filename='log/test.log', level=logging.DEBUG)
+    # logging.basicConfig(filename='log/test.log', level=logging.DEBUG)
+    # logging.critical('critical')
+    # logging.error('error')
+    # logging.warning('warning')
+    # logging.info('info')
+    # logging.debug('debug')
+    # logging.info('info %s %s' % ('test', 'test2'))
 
-    logging.critical('critical')
-    logging.error('error')
-    logging.warning('warning')
-    logging.info('info')
-    logging.debug('debug')
+    formatter = '%(levelname)s: %(asctime)s %(message)s'
+    logging.basicConfig(level=logging.INFO, format=formatter)
 
     logging.info('info %s %s' % ('test', 'test2'))
