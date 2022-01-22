4019ba28a13120a460cd7eb56b48c256b259c861
Shuhei Abe <blueletter123456789@gmail.com>
2022-01-22 18:14:22 +0900

    create pytest config

---
+++ my_test/my_pytest2.py
@@ -0,0 +1,19 @@
+import pytest
+from my_test import calculation
+
+
+class TestCal(object):
+    @classmethod
+    def setup_class(cls):
+        cls.cal = calculation.Cal()
+
+    def test_add_num_and_double(self, request):
+        os_name = request.config.getoption('--os-name')
+        print(os_name)
+        if os_name == 'mac':
+            print('ls')
+        elif os_name == 'windows':
+            print('dir')
+        else:
+            print('ls')
+        assert self.cal.add_num_and_double(1, 1) == 4
