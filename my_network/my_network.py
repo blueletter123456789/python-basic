673298aac91282a87e2e753961246bc2f932a2e5
Shuhei Abe <blueletter123456789@gmail.com>
2022-01-22 16:15:50 +0900

    create requests method

--- my_network/my_network.py
+++ my_network/my_network.py
@@ -1,20 +1,33 @@
 import json
 
+import requests
+
 
 def main():
-    j = {
-        'employee': [
-            {'id': 111, 'name': 'Mike'},
-            {'id': 222, 'name': 'Nancy'}
-        ]
-    }
-    print(j)
-    print('############')
-    print(json.dumps(j))
+    # j = {
+    #     'employee': [
+    #         {'id': 111, 'name': 'Mike'},
+    #         {'id': 222, 'name': 'Nancy'}
+    #     ]
+    # }
+    # print(j)
+    # print('############')
+    # print(json.dumps(j))
+    #
+    # with open('output/test.json', 'w') as f:
+    #     json.dump(j, f)
+    #
+    # print('############')
+    # with open('output/test.json', 'r') as f:
+    #     print(json.load(f))
 
-    with open('output/test.json', 'w') as f:
-        json.dump(j, f)
+    payload = {'key1': 1, 'key2': 2}
+    # r = requests.get('http://httpbin.org/get', params=payload)
+    # r = requests.post('http://httpbin.org/post', data=payload)
+    # r = requests.put('http://httpbin.org/put', data=payload)
+    # r = requests.delete('http://httpbin.org/delete', data=payload)
 
-    print('############')
-    with open('output/test.json', 'r') as f:
-        print(json.load(f))
+    r = requests.get('http://httpbin.org/get', params=payload, timeout=0.01)
+    print(r.status_code)
+    print(r.text)
+    print(r.json())
